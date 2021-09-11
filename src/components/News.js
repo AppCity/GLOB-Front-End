e6118e92cfc23ed986922df188d73342d3378c81
Santhosh Umapathi <Santhosh.Umapathi@app-city.co>
2021-09-11 22:23:34 +0200

    FIX: Blogs card fixed

--- src/components/News.js
+++ src/components/News.js
@@ -18,7 +18,6 @@ const News = ({
     if (state.isUserLoggedIn) {
     } else {
       toast.error("Login to like a post");
-      return;
     }
     console.log("Like pressed");
   };
@@ -36,56 +35,56 @@ const News = ({
   };
 
   return (
-    <div
-      className="bg-white rounded-xl flex bg-opacity-30 backdrop-filter backdrop-blur-md shadow-md hover:shadow-lg transition-all cursor-pointer
-    dark:bg-black dark:bg-opacity-60 
-    "
-      onClick={newsHandler}
-    >
-      <div className="flex h-32 w-32 rounded-xl overflow-hidden sm:w-36 sm:h-36 md:h-44 md:w-44">
-        <img src={image} className="object-cover" />
+    <div>
+      <div
+        className="bg-white rounded-xl flex bg-opacity-30 backdrop-filter backdrop-blur-md shadow-md hover:shadow-lg transition-all cursor-pointer dark:bg-black dark:bg-opacity-60"
+        onClick={newsHandler}
+      >
+        <div className="flex h-32 w-32 rounded-xl overflow-hidden sm:w-36 sm:h-36 md:h-44 md:w-44">
+          <img src={image} className="object-cover" />
+        </div>
+
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col px-5 py-3 space-y-2">
+            <span className="bg-gradient-to-r from-orange to-fucsia text-transparent bg-clip-text bg-blend-soft-light sm:text-xl md:text-2xl lg:text-3xl">
+              {title}
+            </span>
+            <span className="text-xs text-gray-500 dark:text-white sm:text-base lg:text-xl">
+              {headline}
+            </span>
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col px-5 py-3 space-y-2">
-          <span className="bg-gradient-to-r from-orange to-fucsia text-transparent bg-clip-text bg-blend-soft-light sm:text-xl md:text-2xl lg:text-3xl">
-            {title}
-          </span>
-          <span className="text-xs text-gray-500 dark:text-white sm:text-base lg:text-xl">
-            {headline}
+      <div className="flex flex-row justify-between items-center p-5 sm:pb-8 md:pb-10 -mt-12 sm:-mt-16 ml-[105px] sm:ml-[120px] md:ml-[150px]">
+        <div className="flex flex-row justify-center items-center space-x-1 z-50">
+          <LikeIcon
+            size="12"
+            css="sm:w-[16px] sm:h-[16px] lg:w-[20px] lg:h-[20px]"
+            onClick={likeHandler}
+          />
+          <span className="text-xs sm:text-base lg:text-xl">
+            {kFormatter(likes)}
           </span>
         </div>
 
-        <div className="flex flex-row w-full justify-between items-center p-5">
-          <div className="flex flex-row justify-center items-center space-x-1">
-            <LikeIcon
-              size="12"
-              css="sm:w-[16px] sm:h-[16px] lg:w-[20px] lg:h-[20px]"
-              onClick={likeHandler}
-            />
-            <span className="text-xs sm:text-base lg:text-xl">
-              {kFormatter(likes)}
-            </span>
-          </div>
+        <div className="flex flex-row justify-center items-center space-x-1 z-50">
+          <ClockIcon
+            size="12"
+            css="sm:w-[16px] sm:h-[16px] lg:w-[20px] lg:h-[20px]"
+          />
+          <span className="text-xs sm:text-base  lg:text-xl">
+            {timeAgo(timestamp)}
+          </span>
+        </div>
 
-          <div className="flex flex-row justify-center items-center space-x-1">
-            <ClockIcon
-              size="12"
-              css="sm:w-[16px] sm:h-[16px] lg:w-[20px] lg:h-[20px]"
-            />
-            <span className="text-xs sm:text-base  lg:text-xl">
-              {timeAgo(timestamp)}
-            </span>
-          </div>
-
-          <div className="flex flex-row justify-center items-center space-x-1">
-            <BookmarkIcon
-              onClick={bookmarkHandler}
-              size="12"
-              css="sm:w-[16px] sm:h-[16px] lg:w-[20px] lg:h-[20px]"
-              active={bookmarked}
-            />
-          </div>
+        <div className="flex flex-row justify-center items-center space-x-1 z-50">
+          <BookmarkIcon
+            onClick={bookmarkHandler}
+            size="12"
+            css="sm:w-[16px] sm:h-[16px] lg:w-[20px] lg:h-[20px]"
+            active={bookmarked}
+          />
         </div>
       </div>
     </div>
