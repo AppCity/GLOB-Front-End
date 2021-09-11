16f5d1f4ac97e0c4e0dc67fb638bf5f22ea54886
Santhosh Umapathi <Santhosh.Umapathi@app-city.co>
2021-09-11 22:32:31 +0200

    FIX: Menu card fixed

--- src/components/BlogsCard.js
+++ src/components/BlogsCard.js
@@ -4,16 +4,21 @@ import MenuVertical from "./Icons/MenuVertical";
 
 const BlogsCard = ({ image, title, onClick = () => {}, menuClickHandler }) => {
   return (
-    <div className="flex flex-col items-center py-5 my-5" onClick={onClick}>
-      <div className="flex relative rounded-3xl w-40 h-32 overflow-hidden shadow-lg hover:shadow-xl cursor-pointer">
+    <div className="flex flex-col items-center py-5 my-5">
+      <div
+        className="flex relative rounded-3xl w-40 h-32 overflow-hidden shadow-lg hover:shadow-xl cursor-pointer "
+        onClick={onClick}
+      >
         <img src={image} className="object-cover" onClick={onClick} />
 
         <div className="flex items-center justify-between absolute bottom-0 left-0 right-0 text-gray-200 text-xs mx-5 mb-1 z-10">
           <span>{title}</span>
-          <MenuVertical size="10" onClick={menuClickHandler} />
         </div>
 
         <div className="absolute bg-gradient-to-t from-black to-transparent filter bottom-0 h-6 w-full" />
+      </div>
+      <div className="flex w-full justify-end mr-5 -mt-5">
+        <MenuVertical size="10" onClick={menuClickHandler} />
       </div>
     </div>
   );
